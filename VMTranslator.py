2d031ab738e0f0b6d489bf19679ad4490d65f391
Daniel Castro <castrodd@gmail.com>
2020-06-17 09:02:18 -0700

    Remove unused method

--- VMTranslator.py
+++ VMTranslator.py
@@ -3,17 +3,12 @@ import os
 from Parser import Parser
 from CodeWriter import CodeWriter
 
-def createOutputFileName(file):
-    currentFileName = file.split(".")[0]
-    currentFileName = currentFileName.split("/")[-1]
-    outputFileName = currentFileName + ".asm"
-    return outputFileName
-
 def main():
     try:
         inputName = sys.argv[1]
     except IndexError:
         raise SystemExit("Usage: {} <input filename>".format(sys.argv[0]))
+    
     outputFile = inputName.partition(".")[0] + '.asm'
     filename = os.path.basename(inputName).partition(".")[0]
 
